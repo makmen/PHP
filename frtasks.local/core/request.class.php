5e31a67787b85e6223557a53677b69c923712a09
makmen <makmen@inbox.ru>
2017-07-24 19:04:33 +0300

    safe
    safe

---
+++ frtasks.local/core/request.class.php
@@ -0,0 +1,58 @@
+<?php
+
+class Request {
+
+    public static $protocol='http';
+    public static $user_agent;
+    public static $isAjax = false;
+    public static $requestMethodPost = false;
+    
+    private $data;
+    
+    function getData() {
+        return $this->data;
+    }
+        
+    public function __construct() {
+        $this->data = $this->safe($_REQUEST);
+    }
+    
+    public function __get($name) {
+        if (isset($this->data[$name]))
+            return $this->data[$name];
+    }
+
+    public static function inst(){
+        if ( !empty($_SERVER['HTTPS']) && filter_var($_SERVER['HTTPS'], FILTER_VALIDATE_BOOLEAN)) {
+            self::$protocol = 'https';
+        }
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            self::$requestMethodPost = true;
+        }
+        
+        if (isset($_SERVER['HTTP_USER_AGENT'])) {
+            self::$user_agent = $_SERVER['HTTP_USER_AGENT'];
+        }
+        
+	if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && 
+                !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && 
+                strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            self::$isAjax = true;
+        }
+        URL::parse();
+    }
+
+    private function safe($data) {
+        if (is_array($data)) {
+            $escaped = array();
+            foreach ($data as $key => $value) {
+                $escaped[$key] = $this->safe($value);
+            }
+            
+            return $escaped;
+        }
+        
+        return trim( htmlspecialchars($data) );
+    }
+
+}
